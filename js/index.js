aeb90590a615e98a6e49ff6005d0eb4fd086b199
sprait38 <122175964+sprait38@users.noreply.github.com>
2025-01-04 18:54:34 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,44 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 10000); // Меняйте изображение каждые 6 секунды
+function ShowHideDiv(chkPassport) {
+  var dvPassport = document.getElementById("dvPassport");
+dvPassport.style.display = chkPassport.checked ? "block" : "none";
 }
+
+function ShowDiv(chkPassport_2) {
+  var dvPassport_2 = document.getElementById("dvPassport_2");
+dvPassport_2.style.display = chkPassport_2.checked ? "block" : "none";
+}
+
+function Show(chkPassport_3) {
+  var dvPassport_3 = document.getElementById("dvPassport_3");
+dvPassport_3.style.display = chkPassport_3.checked ? "block" : "none";
+}
+
+function Show2(chkPassport_4) {
+  var dvPassport_4 = document.getElementById("dvPassport_4");
+dvPassport_4.style.display = chkPassport_4.checked ? "block" : "none";
+}
+
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
+
+  autoPlay: true,
+  lazyLoad: true, 
+  freeScroll: true, 
+  wrapAround: true, 
+  cellAlign: 'left', 
+  lazyLoad: 2, 
+  initialIndex: 2
+});
+
+var elem_2 = document.querySelector('.slider');
+var flkty = new Flickity( elem_2, {
+
+  autoPlay: true,
+  lazyLoad: true, 
+  freeScroll: true, 
+  wrapAround: true,
+  rightToLeft: true, 
+  cellAlign: 'left', 
+  lazyLoad: 2, 
+  initialIndex: 2
+});
